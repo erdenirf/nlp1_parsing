ed919753f28d2fc6688563e0dbd917aba9fe08e4
Erdeni Tsyrendashiev <erdenirf@gmail.com>
2024-12-17 04:13:56 +0300

    add parse

--- src/utils.py
+++ src/utils.py
@@ -11,9 +11,11 @@ def parse_html(html: str) -> dict:
     """
     soup = BeautifulSoup(html, 'lxml')
 
-    def categories(soup: BeautifulSoup) -> str:
+    def categories(soup: BeautifulSoup, default: str = "N/A") -> str:
         tags = soup.find_all('a', class_='tag__link')
-        return ", ".join([tag.get_text(strip=True) for tag in tags])
+        if len(tags) > 0:
+            return ", ".join([tag.get_text(strip=True) for tag in tags])
+        return default
     
     def tags(soup: BeautifulSoup, default: str = "N/A") -> str:
         tags = soup.find('span', class_='b-singlepost-tags-items')
@@ -31,19 +33,23 @@ def parse_html(html: str) -> dict:
         return default
     
     def body(soup: BeautifulSoup, default: str = "N/A") -> str:
+        body_text = ""
         body = soup.find('article', class_='entry-content')
         if body is not None:
-            return body.get_text(strip=True, separator=" ")
-        body = soup.find('div', class_='aentry-post__content')
-        if body is not None:
-            return body.get_text(strip=True, separator=" ")
+            body_text = body.get_text(strip=True, separator=" ").strip()
+        else:
+            body = soup.find('div', class_='aentry-post__content')
+            if body is not None:
+                body_text = body.get_text(strip=True, separator=" ").strip()
+        if len(body_text) > 0:
+            return body_text
         return default
 
     return {
-        "date": soup.find('time').get_text(strip=True),
+        "date": soup.find('time').get_text(strip=True, separator=" "),
         "title": title(soup),
         "categories": categories(soup),
+        "body": body(soup),
         "tags": tags(soup),
         "url": soup.find('link', attrs={'rel': 'canonical'}).get('href').strip(),
-        "body": body(soup),
     }
